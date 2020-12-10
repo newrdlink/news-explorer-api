711c2d204945c70bd958bb8614fd9bfc3efc087c
Roman Didukh <newrdlink@yandex.ru>
2020-12-10 17:25:49 +0300

    fix auth name 2

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const auth = require('../middlewares/auth');
+const auth = require('../middlewares/authss');
 const usersRoute = require('./users');
 const articlesRoute = require('./articles');
 const { userCreate, login } = require('../controllers/users.js');

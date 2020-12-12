5452d94295ef739b6d45de0473ac50f0d4dfed95
Roman Didukh <newrdlink@yandex.ru>
2020-12-12 15:44:17 +0300

    after first review 2

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 const { NODE_ENV, JWT_SECRET, BD_ADDRESS } = process.env;
 
 const production = () => NODE_ENV === 'production';
+
 const JWT_WORD = production() ? JWT_SECRET : 'The time has come';
 const BD_ADD = production() ? BD_ADDRESS : 'mongodb://localhost:27017/newsexplorerbd';
 
